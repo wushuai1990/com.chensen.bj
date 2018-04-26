2b0139d1f7a60c91ec5075377431f567b326b47b
wushuai <1198788742@qq.com>
2018-04-26 15:39:43 +0800

    修改login,加断言

--- src/test/java/Control/login.java
+++ src/test/java/Control/login.java
@@ -1,6 +1,9 @@
 package Control;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,7 +17,7 @@ public class login {
 	WebDriver driver = null;
 	@BeforeClass
 	public void toInit(){
-		findElem = new FindElem("chrome","http://scm.devc7p.choicesaas.cn");
+		findElem = new FindElem("chrome","http://nboh.choicesaas.cn/index.html#/");
 		driver = findElem.getWebDriver();
 	}
 	
@@ -24,6 +27,11 @@ public class login {
 			log.info("开始登陆");
 			findElem.ByI("userName").sendKeys("15210732928");
 			findElem.ByI("password").sendKeys("123456");
+			Actions action = new Actions(driver);
+			action.sendKeys(Keys.ENTER).perform();
+			String str = findElem.ByX("//*[@id='container']/div/div/div[1]/div/span").getText().toString();
+			Assert.assertEquals(str, "W小店");
+			System.out.println("----str的值:------"+str);
 		}catch(Exception e){
 			log.error("登陆失败");
 		}
